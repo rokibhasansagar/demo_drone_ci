5cffc073b3eb12714d9632a9fe69b19e2a614dfe
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2019-08-30 18:52:49 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=rokibhasansagar
+GITHUB_EMAIL=rokibhasansagar2014@outlook.com
 
-KBUILD_BUILD_USER="Aki"
-KBUILD_BUILD_HOST="A_DEAD_PLANET"
+KBUILD_BUILD_USER="fr3aky"
+KBUILD_BUILD_HOST="phantomzone"
 
-export oem=motorola
-export device=harpia
+export oem=lenovo
+export device=k5fpr
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
 
-release_repo="Akianonymus/harpia_builds"
+release_repo="rokibhasansagar/demo_drone_ci"
 
-timezone="Asia/Kolkata"
+timezone="Asia/Almaty"
